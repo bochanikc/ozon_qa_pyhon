d2bcbe51c8c9f223a5ebb356857a40752f8f08c7
nifrolov <nifrolov@ozon.ru>
2022-05-05 19:30:36 +0300

    [GO] add test "empty id - failed to get item"

--- eight/item-api/test/tests/get_item_test.go
+++ eight/item-api/test/tests/get_item_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/stretchr/testify/require"
 	item "gitlab.ozon.dev/qa/teachers/qa-route-256/item-api/pkg/api"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"testing"
 )
 
@@ -44,5 +46,16 @@ func TestGetItem(t *testing.T) {
 		assert.Equal(t, description, getItemRes.Description)
 		assert.Equal(t, title, getItemRes.Title)
 		t.Logf("id: %v, des: %v, title: %v", id, getItemRes.Description, getItemRes.Title)
+	})
+
+	t.Run("empty id - failed to get item", func(t *testing.T) {
+		//act
+		getItemReq := item.GetItemRequest{}
+		_, err := itemClient.GetItem(ctx, &getItemReq)
+
+		//assert
+		require.Error(t, err)
+		assert.Equal(t, codes.Unknown.String(), status.Code(err).String())
+		t.Logf("Actual error: %v", status.Code(err).String())
 	})
 }
